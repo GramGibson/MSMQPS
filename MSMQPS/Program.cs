161d64d531532f9bed255457da82df6e1766baa8
Gram Gibson <gram.gibson@uky.edu>
2010-07-02 11:26:55 -0400

    Added ReadyQueue method

--- MSMQPS/Program.cs
+++ MSMQPS/Program.cs
@@ -1,63 +1,68 @@
 ﻿namespace MSMQPS {
-    using System;
-    using System.IO;
-    using System.Net.Mail;
-    using System.Timers;
-    using Topshelf;
-    using Topshelf.Configuration;
-    using Topshelf.Configuration.Dsl;
+	using System;
+	using System.Messaging;
+	using System.Net.Mail;
+	using System.Timers;
+	using Topshelf;
+	using Topshelf.Configuration;
+	using Topshelf.Configuration.Dsl;
 
-    internal class Program {
-        static void Main(string[] args) {
-            RunConfiguration cfg = RunnerConfigurator.New(x => {
-                x.AfterStoppingTheHost(h => { Console.WriteLine("AfterStop called invoked, services are stopping"); });
+	internal class Program {
+		static void Main(string[] args) {
+			RunConfiguration cfg = RunnerConfigurator.New(x => {
+				x.AfterStoppingTheHost(h => { Console.WriteLine("AfterStop called invoked, services are stopping"); });
 
-                x.ConfigureServiceInIsolation<MailQueueProcessor>(s => {
-                    s.Named("q");
-                    s.HowToBuildService(name => new MailQueueProcessor());
-                    s.WhenStarted(q => q.Start());
-                    s.WhenStopped(q => q.Stop());
-                });
+				x.ConfigureServiceInIsolation<MailQueueProcessor>(s => {
+					s.Named("q");
+					s.HowToBuildService(name => new MailQueueProcessor());
+					s.WhenStarted(q => q.Start());
+					s.WhenStopped(q => q.Stop());
+				});
 
-                x.RunAsLocalSystem();
+				x.RunAsLocalSystem();
 
-                x.SetDescription("MSMQ-sucking mail sender service.");
-                x.SetDisplayName("MSMQPS");
-                x.SetServiceName("msmqps");
-            });
+				x.SetDescription("MSMQ-sucking mail sender service.");
+				x.SetDisplayName("MSMQPS");
+				x.SetServiceName("msmqps");
+			});
 
-            Runner.Host(cfg, args);
-        }
-    }
+			Runner.Host(cfg, args);
+		}
+	}
 
-    public class MailQueueProcessor {
-        readonly Timer _timer;
+	public class MailQueueProcessor {
+		readonly Timer _timer;
 
-        public MailQueueProcessor() {
-            _timer = new Timer(1000) { AutoReset = true };
-            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine(DateTime.Now);
-        }
+		public MailQueueProcessor() {
+			_timer = new Timer(1000) { AutoReset = true };
+			_timer.Elapsed += (sender, eventArgs) => Console.WriteLine(DateTime.Now);
+		}
 
-        public void Start() {
-            _timer.Start();
-        }
+		public void Start() {
+			_timer.Start();
+		}
 
-        public void Stop() {
-            _timer.Stop();
-        }
+		public void Stop() {
+			_timer.Stop();
+		}
 
-        public void SendMail(IEmail msg) {
-            var mail = new MailMessage();
-            mail.From = new MailAddress(msg.From);
-            mail.Subject = msg.Subject;
-            mail.Body = msg.Body;
-            mail.IsBodyHtml = msg.IsBodyHtml;
+		public static void ReadyQueue(string path) {
+			if (!MessageQueue.Exists(path))
+				MessageQueue.Create(path);
+		}
 
-            foreach (var a in msg.To)
-                mail.To.Add(new MailAddress(a));
+		public void SendMail(IEmail msg) {
+			var mail = new MailMessage();
+			mail.From = new MailAddress(msg.From);
+			mail.Subject = msg.Subject;
+			mail.Body = msg.Body;
+			mail.IsBodyHtml = msg.IsBodyHtml;
 
-            SmtpClient smtp = new SmtpClient("smtpclient");
-            smtp.Send(mail);
-        }
-    }
+			foreach (var a in msg.To)
+				mail.To.Add(new MailAddress(a));
+
+			SmtpClient smtp = new SmtpClient("smtpclient");
+			smtp.Send(mail);
+		}
+	}
 }
