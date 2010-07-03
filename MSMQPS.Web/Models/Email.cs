bd1c3b22a1877ddbe08326df742894041c27efc1
Gram Gibson <gram.gibson@uky.edu>
2010-07-02 23:12:08 -0400

    Added Email validator

--- MSMQPS.Web/Models/Email.cs
+++ MSMQPS.Web/Models/Email.cs
@@ -15,13 +15,19 @@
 	[Serializable]
 	public class Email : IEmail {
 		[Required]
+		[Email(ErrorMessage="This isn't a valid email address.")]
 		public string From { get; set; }
 		[Required]
+		[Email(ErrorMessage = "This isn't a valid email address.")]
 		public string To { get; set; }
 		public string Cc { get; set; }
 		public string Subject { get; set; }
 		public string Body { get; set; }
 		[DisplayName("Send email as HTML?")]
 		public bool IsBodyHtml { get; set; }
+	}
+
+	public class EmailAttribute : RegularExpressionAttribute {
+		public EmailAttribute() : base(@"/^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i") { }
 	}
 }
