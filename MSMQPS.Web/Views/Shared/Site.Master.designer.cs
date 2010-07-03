72c1b3f5920201c84502d1895a88d080c51e0c0e
Gram Gibson <gram.gibson@uky.edu>
2010-07-02 22:18:37 -0400

    Updated Site.Master PlaceHolder ids

--- MSMQPS.Web/Views/Shared/Site.Master.designer.cs
+++ MSMQPS.Web/Views/Shared/Site.Master.designer.cs
@@ -13,21 +13,21 @@ namespace MSMQPS.Web.Views.Shared {
     public partial class Site {
         
         /// <summary>
-        /// head control.
+        /// HeadContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
         
         /// <summary>
-        /// ContentPlaceHolder1 control.
+        /// MainContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
