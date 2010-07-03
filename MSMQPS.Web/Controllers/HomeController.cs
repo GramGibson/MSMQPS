e6a12c52fb82c0dfa4b51920374c0944d93dfba6
Gram Gibson <gram.gibson@uky.edu>
2010-07-02 22:13:42 -0400

    Added Home controller

---
+++ MSMQPS.Web/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿namespace MSMQPS.Web.Controllers {
+	using System.Web.Mvc;
+
+	public class HomeController : Controller {
+		public ActionResult Index() {
+			return RedirectToAction("Create", "Message");
+		}
+	}
+}
