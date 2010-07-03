c9a38ffd48a9f99932bdb676d939086841077bff
Gram Gibson <gram.gibson@uky.edu>
2010-07-02 22:16:27 -0400

    Added Message controller

---
+++ MSMQPS.Web/Controllers/MessageController.cs
@@ -0,0 +1,14 @@
+﻿namespace MSMQPS.Web.Controllers {
+	using System.Web.Mvc;
+
+	public class MessageController : Controller {
+		public ActionResult Create() {
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create(IEmail email) {
+			return RedirectToAction("");
+		}
+	}
+}
